2434eb2ea84e41036aa8f5e21a8a0002832e1652
KamilaMarcon <kamila.marcon@escola.pr.gov.br>
2024-10-02 11:00:17 -0300

    Add files via upload

---
+++ Aula04_JS_Kamila_Valentina/atividade.js
@@ -0,0 +1,14 @@
+//testando loop com while
+
+function testando() {
+    // alert("A casa caiu mano!!!");
+    var valor = prompt("Digite a opção de 1 a 4");
+     console.log(valor);
+}
+
+var x = 0;
+
+while (x <= 5) {
+    document.write('<br>o valor de x é :' + x);
+    x = x + 1;
+}
